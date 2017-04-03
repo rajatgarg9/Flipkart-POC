16ef78d140ee8109fdc44799b3138b71db173df2
SAPIENT\rgar17 <rgarg25@sapient.com>
2017-04-03 20:20:43 +0530

    template making

--- js/product.js
+++ js/product.js
@@ -1,48 +1,54 @@
 "use strict"
-var specData;
-var specPath=productDetails.products[0].specifications;
+var specData,
+    specTemplate,
+    reviewTemplate;
+var specPath = productDetails.products[0].specifications;
 
-class Specification{
-    constructor(specType,featureName,featureValue){
-        this.specType=specType;
-        this.featureName=featureName;
-        this.featureValue=featureValue;
+class Specification {
+    constructor(specType, featureName, featureValue) {
+        this.specType = specType;
+        this.featureName = featureName;
+        this.featureValue = featureValue;
     }
 }
 
-var specData=(specPath=>{
-    let arr=[],
-        counter=0;    
-for(let i=0;i<specPath.length;i++){
-     for(var j=0;j<specPath[i].spec_type_details.length;j++){
-     arr[counter] = new Specification(specPath[i].spec_type,specPath[i].spec_type_details[j].feature_name,specPath[i].spec_type_details[j].feature_value);
-     counter++;
-     }
-}
-return arr;
+var specData = (specPath => {
+    let arr = [],
+        counter = 0;
+    for (let i = 0; i < specPath.length; i++) {
+        for (var j = 0; j < specPath[i].spec_type_details.length; j++) {
+            arr[counter] = new Specification(specPath[i].spec_type, specPath[i].spec_type_details[j].feature_name, specPath[i].spec_type_details[j].feature_value);
+            counter++;
+        }
+    }
+    return arr;
 })(specPath);
 
-// $(document).ready(function(){
-//         console.log('dsf', $("#entry-template"), productDetails);
-//         var source   = $("#entry-template").html();
-//             var template = Handlebars.compile(source);
-//             var context = productDetails;          
-//             var html    = template(context);
-//             $('#bootstrap-overrides').html(html);
-//      })
-     /*var templateScript = Handlebars.compile(template);
+ specTemplate = {
+    raw_temp: document.getElementById("spec-list-template").innerHTML,
+    context: productDetails.products[0],
+    dest_node: document.getElementById("spec-list"),
+    node_position: 'beforeend'
+}
+templateHandler(specTemplate);
+reviewTemplate = {
+    raw_temp: document.getElementById("review-list-template").innerHTML,
+    context: productDetails.products[0],
+    dest_node: document.getElementById("review-sec"),
+    node_position: 'beforeend'
+}
+templateHandler(reviewTemplate);
 
-// When the template is compiled it is added to Handlebars.templates
-            var renderer = Handlebars.templates["templateScript"];
-            
-            // call the compiled function with the template data
-            var result = renderer({productDetails});
-            
-            document.getElementById("bootstrap-overrides").innerHTML=result+document.getElementById("bootstrap-overrides").innerHTML;*/
- var source   = document.getElementById("entry-template").innerHTML;
- var template = Handlebars.compile(source);
-
-var context = {title: "My New Post", body: "This is my first post!"};
- var html    = template(context);
- document.getElementById("bootstrap-overrides").innerHTML+=html;
-
+/*
+* @method:templateHandler
+* @param:{object}templateInfo
+* @desc :insert dynamic html into DOM
+* @return: undefined{undefined}
+*/
+function templateHandler(templateInfo) {
+    var _template,
+        _html;
+    _template = Handlebars.compile(specTemplate.raw_temp);
+    _html = _template(specTemplate.context);
+    specTemplate.dest_node.insertAdjacentHTML(specTemplate.node_position, _html);
+}
