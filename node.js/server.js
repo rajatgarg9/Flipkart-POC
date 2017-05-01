1fb906c94aa1538e07273c61e3dcd7f9403b695c
Rajat Gera <gargrajat961@gmail.com>
2017-05-01 18:38:09 +0530

    next target image

--- node.js/server.js
+++ node.js/server.js
@@ -13,6 +13,22 @@ app.post('/hello', function(req, res){
 	res.send("You just called the post method at '/hello'!\n");
 });
 
+
+app.use('/things', function(req, res, next){
+	console.log("A request for things received at " + Date.now());
+	next();
+});
+
+//Route handler that sends the response
+app.get('/things', function(req, res){
+	res.send(`'Things'
+  <script src="/reload/reload.js"></script>`);	
+});
+
 var server=app.listen(3000);
 console.log(`listening`);
 reload(server,app);
+
+
+
+
